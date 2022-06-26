7eb3f40d9e00f876aa39b2265e116423538057fa
Ivan Korobov <ivan.koroboff@gmail.com>
2022-06-26 17:15:26 +0200

    Screens compatibility Add thousands methods

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -25,22 +25,81 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 10
+        
+        setupSliders()
         slidersAction()
+        
     }
     
+    
+    
     @IBAction func slidersAction () {
-        let colorViewValue = UIColor(
+        
+        roundLabels()
+        viewBackgorundColor()
+        
+        
+    }
+    
+    
+    // MARK: private methods
+    
+    private func setupSliders () {
+        
+        
+        redSlider.minimumTrackTintColor = UIColor(named: "lightRed")
+        redSlider.maximumTrackTintColor = UIColor(named: "darkRed")
+        redSlider.thumbTintColor = UIColor(named: "lightRed")
+        redSlider.value = Float(colorView.backgroundColor?.getColorValue().red ?? 0.25)
+        
+        greenSlider.minimumTrackTintColor = UIColor(named: "lightGreen")
+        greenSlider.maximumTrackTintColor = UIColor(named: "darkGreen")
+        greenSlider.thumbTintColor = UIColor(named: "lightGreen")
+        greenSlider.value = Float(colorView.backgroundColor?.getColorValue().green ?? 0.25)
+        
+        blueSlider.minimumTrackTintColor = UIColor(named: "lightBlue")
+        blueSlider.maximumTrackTintColor = UIColor(named: "darkBlue")
+        blueSlider.thumbTintColor = UIColor(named: "lightBlue")
+        blueSlider.value = Float(colorView.backgroundColor?.getColorValue().blue ?? 0.25)
+        
+    }
+    
+    private func roundLabels () {
+        
+        getRoundedValue(label: redValue, slider: redSlider)
+        getRoundedValue(label: greenValue, slider: greenSlider)
+        getRoundedValue(label: blueValue, slider: blueSlider)
+    }
+    
+    private func getRoundedValue (label: UILabel, slider: UISlider ) {
+        
+        label.text = String(format: "%.2f", slider.value)
+    }
+    
+    private func viewBackgorundColor() {
+        colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
-        colorView.backgroundColor = colorViewValue
-        
-        redValue.text = String(format: "%.2f", redSlider.value)
-        greenValue.text = String(format: "%.2f", greenSlider.value)
-        blueValue.text = String(format: "%.2f", blueSlider.value)
-        
+    }
+    
+}
+
+
+extension UIColor {
+    
+    func getColorValue() -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return (red: red, green: green, blue: blue, alpha: alpha)
     }
 }
+
+
+
 
